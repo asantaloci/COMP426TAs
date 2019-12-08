87ff53f8f94bf68cb8863fd37581f89467c65703
asantaloci <42587263+asantaloci@users.noreply.github.com>
2019-12-08 15:33:10 -0500

    Add files via upload

---
+++ load.js
@@ -0,0 +1,29 @@
+// // click "Run" to replay
+// window.onload = function () {
+//
+// var $body = $('body');
+//
+// var loading = [
+//   { elements: $body, properties: { width: '20%' } },
+//   { elements: $body, properties: { width: '30%' } },
+//   { elements: $body, properties: { width: '50%' } },
+//   { elements: $body, properties: { width: '100%' } },
+//   { elements: $body, properties: { height: '100%' }, options: {
+//     complete: function () {
+//       $('.wrap').velocity( 'transition.slideUpIn' );
+//       $('img').velocity( 'transition.flipYIn' );
+//       $('html').css({ background: '#fff' });
+//     }
+//   }
+// }
+// ];
+//
+// $.Velocity.RunSequence(loading);
+// }
+$(document).ready(function () {
+
+  $('.icon').on('click', function() {
+    $(this).toggleClass('active');
+  });
+
+});
