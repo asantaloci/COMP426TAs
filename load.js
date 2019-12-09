cd366b1e9b8e2c0f555fb78698deeab181f2a5a9
travischeung <46611278+travischeung@users.noreply.github.com>
2019-12-09 10:47:09 -0500

    added a manager for maps

--- load.js
+++ load.js
@@ -1,25 +1,3 @@
-// // click "Run" to replay
-// window.onload = function () {
-//
-// var $body = $('body');
-//
-// var loading = [
-//   { elements: $body, properties: { width: '20%' } },
-//   { elements: $body, properties: { width: '30%' } },
-//   { elements: $body, properties: { width: '50%' } },
-//   { elements: $body, properties: { width: '100%' } },
-//   { elements: $body, properties: { height: '100%' }, options: {
-//     complete: function () {
-//       $('.wrap').velocity( 'transition.slideUpIn' );
-//       $('img').velocity( 'transition.flipYIn' );
-//       $('html').css({ background: '#fff' });
-//     }
-//   }
-// }
-// ];
-//
-// $.Velocity.RunSequence(loading);
-// }
 $(document).ready(function () {
 
   $('.icon').on('click', function() {
@@ -27,3 +5,5 @@ $(document).ready(function () {
   });
 
 });
+
+
